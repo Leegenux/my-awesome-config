8635ed19e776277d396cae3873e0483af342a666
Leegenux <qq1350747952@163.com>
2019-06-12 22:02:08 +0800

    Autostart of flameshot

--- autorun.sh
+++ autorun.sh
@@ -3,3 +3,11 @@
 if [ `pgrep electron-ssr | wc -l` -eq 0 ] ; then   # Make sure that the electron-ssr is under the $PATH
 	electron-ssr &
 fi
+
+if [ `pgrep compton | wc -l` -eq 0 ] ; then   
+	compton &
+fi
+
+if [ `pgrep flameshot | wc -l` -eq 0 ] ; then   
+	flameshot &
+fi
