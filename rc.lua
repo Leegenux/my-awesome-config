3ef71ae793d0786529421ab65333c9e1c9f280f5
Evan <1350747952@qq.com>
2020-12-06 17:46:39 +0800

    code refactored

--- rc.lua
+++ rc.lua
@@ -485,46 +485,48 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     size_hints_honor = false, -- Remove gaps between terminals
-                     screen = awful.screen.preferred,
-                     callback = awful.client.setslave,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+        properties = { border_width = beautiful.border_width,
+                        border_color = beautiful.border_normal,
+                        focus = awful.client.focus.filter,
+                        raise = true,
+                        keys = clientkeys,
+                        buttons = clientbuttons,
+                        size_hints_honor = false, -- Remove gaps between terminals
+                        screen = awful.screen.preferred,
+                        callback = awful.client.setslave,
+                        placement = awful.placement.no_overlap+awful.placement.no_offscreen
+        }
     },
 
     -- Floating clients.
     { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-        },
-        class = {
-          "Arandr",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Wpa_gui",
-          "pinentry",
-          "veromix",
-          "xtightvncviewer",
-          "Guake"
-        },
+            instance = {
+            "DTA",  -- Firefox addon DownThemAll.
+            "copyq",  -- Includes session name in class.
+            },
+            class = {
+            "Arandr",
+            "Gpick",
+            "Kruler",
+            "MessageWin",  -- kalarm.
+            "Sxiv",
+            "Wpa_gui",
+            "pinentry",
+            "veromix",
+            "xtightvncviewer",
+            "Guake"
+            },
 
-        name = {
-          "Event Tester",  -- xev.
+            name = {
+            "Event Tester",  -- xev.
+            },
+            role = {
+            "AlarmWindow",  -- Thunderbird's calendar.
+            "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            }
         },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
+        properties = { floating = true }
+    },
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" } },
